0fc24b3481add8aee086bd29fc83afdf56505877
David <matousch.david@gmail.com>
2022-06-08 14:20:04 +0200

    Initial commit

---
+++ src/main/java/spw4/game2048/GameImpl.java
@@ -0,0 +1,130 @@
+package spw4.game2048;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.function.Function;
+
+public class GameImpl implements Game {
+    public static Random random;
+    private final int size = 4;
+    private final int[][] board;
+    private int score;
+
+    public GameImpl() {
+        board = new int[size][size];
+    }
+
+    public int getMoves() {
+        // to do ...
+        return 0;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getValueAt(int x, int y) {
+        return board[y][x];
+    }
+
+
+    public boolean isOver() {
+        for (int i = 0; i < size; i++) {
+            for (int o = 0; o < size; o++) {
+                if (board[i][o] == 0) return false;
+
+                if ((o < size - 1 && board[i][o+1] == board[i][o])
+                        || (o > 0 && board[i][o-1] == board[i][o])
+                        || (i < size -1 && board[i+1][o] == board[i][o])
+                        || (i > 0 && board[i-1][o] == board[i][o])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isWon() {
+        // to do ...
+        return false;
+    }
+
+    @Override
+    public int getBoardSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        // to do ...
+        return "";
+    }
+
+    public void initialize() {
+        placeRandomTile();
+        placeRandomTile();
+    }
+
+    public void move(Direction direction) {
+        boolean isVertical = direction == Direction.up || direction == Direction.down;
+
+        int startValue = 0;
+        int increment = 1;
+        Function<Integer, Boolean> condition = (Integer i) -> i < size;
+
+        switch (direction) {
+            case right, down -> {
+                startValue = size - 1;
+                increment = -1;
+                condition = (Integer i) -> i >= 0;
+            }
+        }
+
+        var summedUpFields = new boolean[size][size];
+        for (int yPosToShiftTo = startValue; condition.apply(yPosToShiftTo); yPosToShiftTo += increment) {
+            for (int xPosToShiftTo = startValue; condition.apply(xPosToShiftTo); xPosToShiftTo += increment) {
+                for (int cursorPos = isVertical ? yPosToShiftTo : xPosToShiftTo; condition.apply(cursorPos); cursorPos += increment) {
+                    int yPosToShiftFrom = yPosToShiftTo;
+                    int xPosToShiftFrom = cursorPos;
+
+
+                    if (isVertical) {
+                        xPosToShiftFrom = xPosToShiftTo;
+                        yPosToShiftFrom = cursorPos;
+                    }
+                    if(yPosToShiftFrom == yPosToShiftTo && xPosToShiftFrom == xPosToShiftTo)
+                        continue;
+                    if (board[yPosToShiftFrom][xPosToShiftFrom] != 0) {
+                        if (board[yPosToShiftTo][xPosToShiftTo] == board[yPosToShiftFrom][xPosToShiftFrom] && !summedUpFields[yPosToShiftTo][xPosToShiftTo]) {
+                            board[yPosToShiftTo][xPosToShiftTo] *= 2;
+                            board[yPosToShiftFrom][xPosToShiftFrom] = 0;
+                            score += board[yPosToShiftTo][xPosToShiftTo];
+                            summedUpFields[yPosToShiftTo][xPosToShiftTo] = true;
+                        } else if(board[yPosToShiftTo][xPosToShiftTo] == 0) {
+                            board[yPosToShiftTo][xPosToShiftTo] = board[yPosToShiftFrom][xPosToShiftFrom];
+                            board[yPosToShiftFrom][xPosToShiftFrom] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        placeRandomTile();
+    }
+
+    public void placeRandomTile() {
+        int nextX;
+        int nextY;
+        do {
+            nextY = random.nextInt(size);
+            nextX = random.nextInt(size);
+        } while (board[nextY][nextX] != 0);
+
+        board[nextY][nextX] = random.nextInt(10) == 0 ? 4 : 2;
+    }
+
+    public void placeTile(int x, int y, int value) {
+
+        board[y][x] = value;
+    }
+}
---
+++ src/test/java/spw4/game2048/GameTest.java
@@ -0,0 +1,364 @@
+package spw4.game2048;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class GameTest {
+    class RandomStub extends Random {
+        public RandomStub(int[] returnVals) {
+            this.returnVals = returnVals;
+        }
+
+        public RandomStub() {
+        }
+
+        int[] returnVals = new int[]{1, 1, 0, 1, 2, 1, 0, 0, 0};
+        int counter = 0;
+
+        @Override
+        public int nextInt(int bound) {
+            return returnVals[counter++];
+        }
+    }
+
+    @BeforeEach
+    void setupTest() {
+        GameImpl.random = new Random();
+    }
+
+    @DisplayName("Init")
+    @Nested
+    class InitTests {
+        @Test
+        void createBoardReturns4By4Field() {
+            var sut = new GameImpl();
+            sut.initialize();
+            var expectedSize = 4;
+
+            int boardSize = sut.getBoardSize();
+
+            assertEquals(expectedSize, boardSize);
+        }
+
+        @Test
+        void createBoardReturnsBoardWithSum4or6() {
+            var sut = new GameImpl();
+            sut.initialize();
+            int[] expected = new int[]{4, 6, 8};
+
+            int sum = getBoardSum(sut);
+
+            assertThat(expected).contains(sum);
+        }
+
+        @Test
+        void createBoardUsesRandom() {
+            GameImpl.random = spy(Random.class);
+            var sut = new GameImpl();
+
+            sut.initialize();
+
+            verify(GameImpl.random);
+        }
+
+        /**
+         * Creates this board:
+         * <p>
+         * 0 | 0 | 0 | 0
+         * 0 | 4 | 2 | 0
+         * 0 | 0 | 0 | 0
+         * 0 | 0 | 0 | 0
+         */
+        @Test
+        void createBoardUsingRandomStubReturnsBoardWithOne4One2() {
+            GameImpl.random = new RandomStub();
+            var sut = new GameImpl();
+            sut.initialize();
+            var expectedSum = 6;
+            var expectedFirst = 4;
+            var expectedSecond = 2;
+
+            assertEquals(expectedSum, getBoardSum(sut));
+            assertEquals(expectedFirst, sut.getValueAt(1, 1));
+            assertEquals(expectedSecond, sut.getValueAt(2, 1));
+        }
+
+        @Test
+        void createBoardReturnsNotWonYet() {
+            GameImpl.random = new RandomStub();
+            var sut = new GameImpl();
+            sut.initialize();
+            var expected = false;
+
+            assertEquals(expected, sut.isWon());
+        }
+
+        @Test
+        void createBoardReturnsNotGameOverYet() {
+            GameImpl.random = new RandomStub();
+            var sut = new GameImpl();
+            sut.initialize();
+            var expected = false;
+
+            assertEquals(expected, sut.isOver());
+        }
+
+        @Test
+        void createFullBoardReturnsGameOver() {
+            GameImpl.random = new RandomStub();
+            var sut = new GameImpl();
+            for (int i = 0; i < sut.getBoardSize(); i++) {
+                for (int k = 0; k < sut.getBoardSize(); k++) {
+                        sut.placeTile(i, k, ((i + k) % 2 == 0) ? 4 : 2);
+                }
+            }
+            var expected = true;
+
+            assertEquals(expected, sut.isOver());
+        }
+
+        private int getBoardSum(GameImpl sut) {
+            int sum = 0;
+            for (int i = 0; i < sut.getBoardSize(); i++) {
+                for (int o = 0; o < sut.getBoardSize(); o++) {
+                    sum += sut.getValueAt(i, o);
+                }
+            }
+            return sum;
+        }
+    }
+
+
+    @DisplayName("Move")
+    @Nested
+    class MoveTests {
+        @Test
+        void moveRightMovesTilesToRight() {
+            GameImpl.random = new RandomStub();
+            var sut = new GameImpl();
+            sut.initialize();
+            var expectedFirst = 4;
+            var expectedSecond = 2;
+
+            sut.move(Direction.right);
+
+            assertEquals(expectedFirst, sut.getValueAt(sut.getBoardSize() - 2, 1));
+            assertEquals(expectedSecond, sut.getValueAt(sut.getBoardSize() - 1, 1));
+        }
+
+        @Test
+        void moveLeftMovesTilesToLeft() {
+            GameImpl.random = new RandomStub();
+            var sut = new GameImpl();
+            sut.initialize();
+            var expectedFirst = 4;
+            var expectedSecond = 2;
+
+            sut.move(Direction.left);
+
+            assertEquals(expectedFirst, sut.getValueAt(0, 1));
+            assertEquals(expectedSecond, sut.getValueAt(1, 1));
+        }
+
+        @Test
+        void moveUpMovesTilesUp() {
+            GameImpl.random = new RandomStub();
+            var sut = new GameImpl();
+            sut.initialize();
+            var expectedFirst = 4;
+            var expectedSecond = 2;
+
+            sut.move(Direction.up);
+
+            assertEquals(expectedFirst, sut.getValueAt(1, 0));
+            assertEquals(expectedSecond, sut.getValueAt(2, 0));
+        }
+
+        @Test
+        void moveDownMovesTilesDown() {
+            GameImpl.random = new RandomStub();
+            var sut = new GameImpl();
+            sut.initialize();
+            var expectedFirst = 4;
+            var expectedSecond = 2;
+
+            sut.move(Direction.down);
+
+            assertEquals(expectedFirst, sut.getValueAt(1, sut.getBoardSize() - 1));
+            assertEquals(expectedSecond, sut.getValueAt(2, sut.getBoardSize() - 1));
+        }
+
+        @Test
+        void moveCreatesRandomTile() {
+            GameImpl.random = new RandomStub(new int[]{1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 3, 1});
+            var sut = spy(GameImpl.class);
+            sut.initialize();
+
+            sut.move(Direction.down);
+
+            verify(sut, times(3)).placeRandomTile();
+        }
+    }
+
+    @DisplayName("Add")
+    @Nested
+    class AddTests {
+        @Test
+        void moveLeftTwo2InMiddleAddsTilesTogether() {
+            GameImpl.random = new RandomStub(new int[]{1, 1, 1, 1, 2, 1, 0, 0, 0});
+            var sut = new GameImpl();
+            sut.initialize();
+            var expected = 4;
+
+            sut.move(Direction.left);
+
+            assertEquals(expected, sut.getValueAt(0, 1));
+        }
+
+        @Test
+        void moveRightTwo2InMiddleAddsTilesTogether() {
+            GameImpl.random = new RandomStub(new int[]{1, 1, 1, 1, 2, 1, 0, 0, 0});
+            var sut = new GameImpl();
+            sut.initialize();
+            var expected = 4;
+
+            sut.move(Direction.right);
+
+            assertEquals(expected, sut.getValueAt(sut.getBoardSize() - 1, 1));
+        }
+
+        @Test
+        void moveRightTwo2OnRightAddsTilesTogether() {
+            GameImpl.random = new RandomStub(new int[]{1, 2, 1, 1, 3, 1, 0, 0, 0});
+            var sut = new GameImpl();
+            sut.initialize();
+            var expected = 4;
+
+            sut.move(Direction.right);
+
+            assertEquals(expected, sut.getValueAt(sut.getBoardSize() - 1, 1));
+        }
+
+        @Test
+        void moveLeftTwo2OnRightAddsTilesTogether() {
+            GameImpl.random = new RandomStub(new int[]{1, 2, 1, 1, 3, 1, 0, 0, 0});
+            var sut = new GameImpl();
+            sut.initialize();
+            var expected = 4;
+
+            sut.move(Direction.left);
+
+            assertEquals(expected, sut.getValueAt(0, 1));
+        }
+
+        @Test
+        void moveLeftTwo2OnLeftAddsTilesTogether() {
+            GameImpl.random = new RandomStub(new int[]{1, 0, 1, 1, 1, 1, 0, 0, 0});
+            var sut = new GameImpl();
+            sut.initialize();
+            var expected = 4;
+
+            sut.move(Direction.left);
+
+            assertEquals(expected, sut.getValueAt(0, 1));
+        }
+
+        @Test
+        void moveLeft4TwosAddsTilesTogether() {
+            GameImpl.random = new RandomStub(new int[]{1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 3, 1, 0, 0, 0});
+            var sut = new GameImpl();
+            sut.initialize();
+            var expectedFirst = 4;
+            var expectedSecond = 4;
+
+            sut.placeRandomTile();
+            sut.placeRandomTile();
+            sut.move(Direction.left);
+
+            assertEquals(expectedFirst, sut.getValueAt(0, 1));
+            assertEquals(expectedSecond, sut.getValueAt(1, 1));
+        }
+
+        @Test
+        void moveLeft2TwosOne4AddsTilesTogether() {
+            GameImpl.random = new RandomStub(new int[]{1, 0, 1, 1, 1, 1, 1, 2, 0, 0, 0, 0});
+            var sut = new GameImpl();
+            sut.initialize();
+            var expectedFirst = 4;
+            var expectedSecond = 4;
+
+            sut.placeRandomTile();
+            sut.move(Direction.left);
+
+            assertEquals(expectedFirst, sut.getValueAt(0, 1));
+            assertEquals(expectedSecond, sut.getValueAt(1, 1));
+        }
+
+        @Test
+        void moveUp2TwosOne4AddsTilesTogether() {
+            GameImpl.random = new RandomStub(new int[]{0, 1, 1, 1, 1, 1, 3, 1, 0, 0, 0, 0});
+            var sut = new GameImpl();
+            sut.initialize();
+            var expectedFirst = 4;
+            var expectedSecond = 4;
+
+            sut.placeRandomTile();
+            sut.move(Direction.up);
+
+            assertEquals(expectedFirst, sut.getValueAt(1, 0));
+            assertEquals(expectedSecond, sut.getValueAt(1, 1));
+        }
+
+        @Test
+        void moveDown2TwosOne4AddsTilesTogether() {
+            GameImpl.random = new RandomStub(new int[]{0, 1, 1, 1, 1, 1, 3, 1, 0, 0, 0, 0});
+            var sut = new GameImpl();
+            sut.initialize();
+            var expectedFirst = 4;
+            var expectedSecond = 4;
+
+            sut.placeRandomTile();
+            sut.move(Direction.down);
+
+            assertEquals(expectedFirst, sut.getValueAt(1, sut.getBoardSize() - 2));
+            assertEquals(expectedSecond, sut.getValueAt(1, sut.getBoardSize() - 1));
+        }
+    }
+
+    @DisplayName("Score")
+    @Nested
+    class ScoreTests {
+        @Test
+        void moveDown2TwosOne4AddsTilesTogether() {
+            GameImpl.random = new RandomStub(new int[]{0, 1, 1, 1, 1, 1, 0, 0, 0});
+            var sut = new GameImpl();
+            sut.initialize();
+            var expectedScore = 4;
+
+            sut.move(Direction.down);
+
+            assertEquals(expectedScore, sut.getScore());
+        }
+
+        @Test
+        void moveMulitpleTimesAddsToScore() {
+            GameImpl.random = new RandomStub(new int[]{0, 1, 1, 1, 1, 1, 3, 2, 0, 0, 0, 0});
+            var sut = new GameImpl();
+            sut.initialize();
+            var expectedScore = 12;
+
+            sut.move(Direction.down);
+            sut.move(Direction.left);
+
+            assertEquals(expectedScore, sut.getScore());
+        }
+    }
+}
